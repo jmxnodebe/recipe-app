7fa925a7a27c3b43ac494c4500120deaa9d4e233
donttestnodi <53544100+jmxnodebe@users.noreply.github.com>
2020-07-10 01:50:27 +0100

    Updates

---
+++ precache-manifest.5cd3da7b9e701729b13f44286a7e6dc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd4468dcd188b2495afbdccdbb334a7",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "bb55b0fc2cf02b38894c",
+    "url": "/recipe-app/static/css/main.b88c9b9c.chunk.css"
+  },
+  {
+    "revision": "b7771c6b7375826e66ee",
+    "url": "/recipe-app/static/js/2.953e3c0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.953e3c0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb55b0fc2cf02b38894c",
+    "url": "/recipe-app/static/js/main.d9af638c.chunk.js"
+  },
+  {
+    "revision": "cd77bb1956273d314d45",
+    "url": "/recipe-app/static/js/runtime-main.14680ac4.js"
+  }
+]);
